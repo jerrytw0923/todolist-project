4c1e7b47bd3182fe1f5bad641bb8a173428169ac
jerry <jerry@gmail.com>
2025-05-14 21:42:53 +0800

    完成base.html

--- user/urls.py
+++ user/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import user_register, user_login
+from .views import user_register, user_login, user_logout
 
 urlpatterns = [
+    path("logout/", user_logout, name="logout"),
     path("login/", user_login, name="login"),
     path("register/", user_register, name="register"),
 ]
