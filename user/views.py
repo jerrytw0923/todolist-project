4714e21b117f29766340cfb5f0e6d5cd9dea2ec1
jerry <jerry@gmail.com>
2025-05-16 19:11:06 +0800

    完成使用者功能

--- user/views.py
+++ user/views.py
@@ -50,6 +50,7 @@ def user_register(request):
                 user = User.objects.create_user(username=username, password=password1)
                 user.save()
                 message = "註冊成功~!"
+                return redirect("login")
     else:
         form = UserCreationForm()
 
