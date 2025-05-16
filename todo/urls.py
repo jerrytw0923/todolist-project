f5998154b77dee60379a6ca4cb92bb527e6400e0
jerry <jerry@gmail.com>
2025-05-16 19:58:00 +0800

    完成建立代辦事項功能

--- todo/urls.py
+++ todo/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import todolist, viewtodo
+from .views import todolist, viewtodo, createtodo
 
 urlpatterns = [
     path("", todolist, name="todolist"),
     path("view/<int:id>/", viewtodo, name="viewtodo"),
+    path("create/", createtodo, name="createtodo"),
 ]
