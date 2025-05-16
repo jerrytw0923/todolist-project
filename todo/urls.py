44305cd77394016ed4aa50c02a168e628263ea06
jerry <jerry@gmail.com>
2025-05-16 21:38:31 +0800

    完成代辦事項更新跟刪除功能

--- todo/urls.py
+++ todo/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import todolist, viewtodo, createtodo
+from .views import todolist, viewtodo, createtodo, deletetodo
 
 urlpatterns = [
     path("", todolist, name="todolist"),
     path("view/<int:id>/", viewtodo, name="viewtodo"),
     path("create/", createtodo, name="createtodo"),
+    path("delete/<int:id>/", deletetodo, name="deletetodo"),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Todo
 from .forms import TodoForm
+from datetime import datetime
 
 
 # Create your views here.
@@ -8,21 +9,45 @@ def todolist(request):
     user = request.user
     todos = None
     if user.is_authenticated:
-        todos = Todo.objects.filter(user=request.user)
+        todos = Todo.objects.filter(user=request.user).order_by("-created")
 
     print(todos)
     result = {"todos": todos, "user": user}
     return render(request, "todo/todolist.html", result)
 
 
+def deletetodo(request, id):
+    todo = Todo.objects.get(id=id)
+    todo.delete()
+    return redirect("todolist")
+
+
 def viewtodo(request, id):
     todo = None
+    form = None
+    message = ""
     try:
         todo = Todo.objects.get(id=id)
+
+        if request.method == "GET":
+            form = TodoForm(instance=todo)
+        else:
+            form = TodoForm(request.POST, instance=todo)
+            todo = form.save(commit=False)
+            if todo.completed:
+                todo.date_completed = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            else:
+                todo.date_completed = None
+
+            todo.save()
+            message = "修改成功!"
+
     except Exception as e:
         print(e)
 
-    return render(request, "todo/viewtodo.html", {"todo": todo})
+    return render(
+        request, "todo/viewtodo.html", {"todo": todo, "form": form, "message": message}
+    )
 
 
 def createtodo(request):
@@ -31,6 +56,9 @@ def createtodo(request):
         todo = form.save(commit=False)
         todo.user = request.user
         todo.save()
+
+        return redirect("todolist")
+
     else:
         form = TodoForm()
 
